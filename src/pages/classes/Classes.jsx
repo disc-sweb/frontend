543906f5824481a4fcfa88571a6d9922bf48104a
Zyanik <gerardoperez8a@gmail.com>
2025-03-01 14:37:57 -0600

    Basic css created

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ClassesStyling = styled.div`
+  h1 {
+    text-align: center;
+  }
+
+  .courses-countainer {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;   
+  }
+`;
 
 const Classes = () => {
   return (
-    <div>Classes</div>
-  )
-}
+    <ClassesStyling>
+      <h1>Classes</h1>
+      <div className='courses-countainer'></div>
+    </ClassesStyling>
+  );
+};
 
-export default Classes
+
+
+export default Classes;

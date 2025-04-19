7ddac25ad7367bcc4ee2939ad61b1eeb74e3aedd
portialii89@gmail.com <portiali@dhcp-10-105-254-103.wireless.northwestern.private>
2025-04-19 12:23:16 -0500

    removed reigstration page from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ import RequestPasswordReset from 'pages/account/RequestPasswordReset';
 import ResetPassword from 'pages/account/ResetPassword';
 import SignUp from 'pages/account/SignUp';
 import Classes from 'pages/courses/Classes';
+import Register from 'pages/courses/Register';
 import Home from 'pages/home/Home';
 import NotFound from 'pages/not-found/NotFound';
-// import Register from 'pages/courses/Register';
 
 import './App.css';
 
